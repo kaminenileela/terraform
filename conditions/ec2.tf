787ae1c5aab881bf32eb05d056bb1a21a57adfed
kaminenileela <kaminenileela1992@email.com>
2024-05-14 10:48:34 -0400

    terraform

---
+++ conditions/ec2.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "db" {
+    ami = var.image_id
+    vpc_security_group_ids = [aws_security_group.allow_ssh1.id]
+    instance_type = var.instance_name == "db" ? "t3.micro" : "t2.micro"
+    
+}
+
+
+
+resource "aws_security_group" "allow_ssh" {
+
+    name = "allow_sg"
+    description = "creating security group"
+
+        ingress {
+
+            from_port = 22
+            to_port = 22
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    
+        egress {
+
+            from_port = 0 
+            to_port = 0
+            protocol = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+        tags = {
+
+            name = "allow_sg"
+            createdby = "Leela"
+        }
+
+
+
+}
